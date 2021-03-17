03e11f25ff7cb68a3e79700b1422d23091133ed1
rodsecco <rodsecco@gmail.com>
2021-03-16 22:07:52 -0300

    Ajustes com o pacote

--- Hypotenuse.java
+++ Hypotenuse.java
@@ -6,20 +6,20 @@ public class Hypotenuse {
 		return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 	}
 
-	 public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-		 
-		 System.out.println("Calculadora de Pitágoras!");
-		 System.out.println("Informe o valor de um dos catetos \n A: ");
-		 int value_a = sc.nextInt();
-		 		 
-		 System.out.println("Informe o valor do outro cateto \n B: ");
-		 int value_b = sc.nextInt();
-		 
-		 Hypotenuse hypotenuse = new Hypotenuse();
-		 
-		 double result = hypotenuse.getHypotenuse(value_a, value_b);
-		 
-		 System.out.println("O valor da hipotenusa e: " + result);
-	 }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Calculadora de Pitagoras!");
+		System.out.print("Informe o valor de um dos catetos \n A: ");
+		int value_a = sc.nextInt();
+				
+		System.out.print("Informe o valor do outro cateto \n B: ");
+		int value_b = sc.nextInt();
+		
+		Hypotenuse hypotenuse = new Hypotenuse();
+		
+		double result = hypotenuse.getHypotenuse(value_a, value_b);
+		
+		System.out.println("O valor da hipotenusa e: " + result);
+	}
 }
